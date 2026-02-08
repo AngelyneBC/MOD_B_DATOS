3753d04cdfe7c5cd5b4a2669850f69571f25f622
20241534-droid <20241534@aloe.ulima.edu.pe>
2026-02-08 13:41:21 -0500

    Cambios -se corrigio el 015 -se agregó el inserts_vehiculos

--- Entidades/015_crear_vehiculos.sql
+++ Entidades/015_crear_vehiculos.sql
@@ -8,7 +8,7 @@ CREATE TABLE vehiculos (
     cliente_id INTEGER,
     
     FOREIGN KEY (modelo_id) REFERENCES modelos(id_modelo),
-    FOREIGN KEY (gps_id) REFERENCES gps(id_gps),);
+    FOREIGN KEY (gps_id) REFERENCES gps(id_gps),
     FOREIGN KEY (cliente_id) REFERENCES clientes(id_cliente),       
     FOREIGN KEY (color_id) REFERENCES color(id_color)
     );
