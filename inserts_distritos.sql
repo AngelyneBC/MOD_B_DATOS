93529401249c62ef0fc0223dab73452708a619be
20241534-droid <20241534@aloe.ulima.edu.pe>
2026-02-10 23:44:11 -0500

    Modificacion de vehiculos y distritos

--- inserts_distritos.sql
+++ inserts_distritos.sql
@@ -1,21 +1,27 @@
 -- migrate:up
 
-INSERT INTO distritos (id_distrito, nombre) VALUES (1, 'ATE');
-INSERT INTO distritos (id_distrito, nombre) VALUES (2, 'CAMACHO');
-INSERT INTO distritos (id_distrito, nombre) VALUES (3, 'CAMACHO PATIO');
-INSERT INTO distritos (id_distrito, nombre) VALUES (4, 'COCHERA');
-INSERT INTO distritos (id_distrito, nombre) VALUES (5, 'EN MOLINA');
-INSERT INTO distritos (id_distrito, nombre) VALUES (6, 'FRONTIS');
-INSERT INTO distritos (id_distrito, nombre) VALUES (7, 'PATIO');
-INSERT INTO distritos (id_distrito, nombre) VALUES (8, 'PATIO CAMACHO');
-INSERT INTO distritos (id_distrito, nombre) VALUES (9, 'PATIO LM');
-INSERT INTO distritos (id_distrito, nombre) VALUES (10, 'PATIO SI');
-INSERT INTO distritos (id_distrito, nombre) VALUES (11, 'PROBLEMAS');
-INSERT INTO distritos (id_distrito, nombre) VALUES (12, 'RETIRO DIRECTO');
-INSERT INTO distritos (id_distrito, nombre) VALUES (13, 'SAN ISIDRO');
-INSERT INTO distritos (id_distrito, nombre) VALUES (14, 'SAN ISIDRO COCHERA ARAÑON EN EL PARACHOQUE DELANTERO');
-INSERT INTO distritos (id_distrito, nombre) VALUES (15, 'SAN ISIDRO SHOWROOM');
-INSERT INTO distritos (id_distrito, nombre) VALUES (16, 'UNIDAD EN PATIO SI');
-
--- migrate:down
-DELETE FROM distritos;
+INSERT INTO distritos (id_distrito, nombre) VALUES (1, 'Ate');
+INSERT INTO distritos (id_distrito, nombre) VALUES (2, 'Barranco');
+INSERT INTO distritos (id_distrito, nombre) VALUES (3, 'Breña');
+INSERT INTO distritos (id_distrito, nombre) VALUES (4, 'Callao');
+INSERT INTO distritos (id_distrito, nombre) VALUES (5, 'Carabayllo');
+INSERT INTO distritos (id_distrito, nombre) VALUES (6, 'Chorrillos');
+INSERT INTO distritos (id_distrito, nombre) VALUES (7, 'Comas');
+INSERT INTO distritos (id_distrito, nombre) VALUES (8, 'Independencia');
+INSERT INTO distritos (id_distrito, nombre) VALUES (9, 'Jesús María');
+INSERT INTO distritos (id_distrito, nombre) VALUES (10, 'La Molina');
+INSERT INTO distritos (id_distrito, nombre) VALUES (11, 'Lince');
+INSERT INTO distritos (id_distrito, nombre) VALUES (12, 'Los Olivos');
+INSERT INTO distritos (id_distrito, nombre) VALUES (13, 'Magdalena del Mar');
+INSERT INTO distritos (id_distrito, nombre) VALUES (14, 'Miraflores');
+INSERT INTO distritos (id_distrito, nombre) VALUES (15, 'Pueblo Libre');
+INSERT INTO distritos (id_distrito, nombre) VALUES (16, 'Puente Piedra');
+INSERT INTO distritos (id_distrito, nombre) VALUES (17, 'Rímac');
+INSERT INTO distritos (id_distrito, nombre) VALUES (18, 'San Borja');
+INSERT INTO distritos (id_distrito, nombre) VALUES (19, 'San Isidro');
+INSERT INTO distritos (id_distrito, nombre) VALUES (20, 'San Juan de Lurigancho');
+INSERT INTO distritos (id_distrito, nombre) VALUES (21, 'San Juan de Miraflores');
+INSERT INTO distritos (id_distrito, nombre) VALUES (22, 'San Miguel');
+INSERT INTO distritos (id_distrito, nombre) VALUES (23, 'Santiago de Surco');
+INSERT INTO distritos (id_distrito, nombre) VALUES (24, 'Villa El Salvador');
+INSERT INTO distritos (id_distrito, nombre) VALUES (25, 'Villa María del Triunfo');
