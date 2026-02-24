fa0eab43c53e0c672c0ee4a54294a9c973c58b15
AngelyneBC <angelynealvarez00@gmail.com>
2026-02-23 19:16:36 -0500

    correcciones inserts

--- inserts_campaña.sql
+++
@@ -1,9 +0,0 @@
--- migrate:up
-
-INSERT INTO campañas (id_campaña, nombre, descuento) VALUES (1, 'Sin campaña', 0.00);
-INSERT INTO campañas (id_campaña, nombre, descuento) VALUES (2, 'Campaña estándar', 0.10);
-INSERT INTO campañas (id_campaña, nombre, descuento) VALUES (3, 'Campaña especial', 0.20);
-INSERT INTO campañas (id_campaña, nombre, descuento) VALUES (4, 'Campaña temporada', 0.15);
-
--- migrate:down
-DELETE FROM campañas;
--- inserts_colors.sql
+++ inserts_colors.sql
@@ -1,21 +1,21 @@
 -- migrate:up
 
-INSERT INTO color (id_color, nombre) VALUES (1, 'AZUL');
-INSERT INTO color (id_color, nombre) VALUES (2, 'BLANCO');
-INSERT INTO color (id_color, nombre) VALUES (3, 'CELESTE');
-INSERT INTO color (id_color, nombre) VALUES (4, 'CREMA');
-INSERT INTO color (id_color, nombre) VALUES (5, 'CREMA/BEIGE');
-INSERT INTO color (id_color, nombre) VALUES (6, 'CREMA/DUA BEIGE');
-INSERT INTO color (id_color, nombre) VALUES (7, 'GRIS');
-INSERT INTO color (id_color, nombre) VALUES (8, 'GRIS PERLA');
-INSERT INTO color (id_color, nombre) VALUES (9, 'GRIS/AZUL');
-INSERT INTO color (id_color, nombre) VALUES (10, 'NARANJA');
-INSERT INTO color (id_color, nombre) VALUES (11, 'NEGRO');
-INSERT INTO color (id_color, nombre) VALUES (12, 'NEGRO AZABACHE');
-INSERT INTO color (id_color, nombre) VALUES (13, 'PLATA');
-INSERT INTO color (id_color, nombre) VALUES (14, 'PLATA CLARO');
-INSERT INTO color (id_color, nombre) VALUES (15, 'ROJO');
-INSERT INTO color (id_color, nombre) VALUES (16, 'VERDE');
+INSERT INTO color (id_color, nombre) VALUES (1, 'GRIS');
+INSERT INTO color (id_color, nombre) VALUES (2, 'NEGRO');
+INSERT INTO color (id_color, nombre) VALUES (3, 'AZUL');
+INSERT INTO color (id_color, nombre) VALUES (4, 'BLANCO');
+INSERT INTO color (id_color, nombre) VALUES (5, 'PLATA CLARO');
+INSERT INTO color (id_color, nombre) VALUES (6, 'ROJO');
+INSERT INTO color (id_color, nombre) VALUES (7, 'CELESTE');
+INSERT INTO color (id_color, nombre) VALUES (8, 'PLATA');
+INSERT INTO color (id_color, nombre) VALUES (9, 'VERDE');
+INSERT INTO color (id_color, nombre) VALUES (10, 'NEGRO AZABACHE');
+INSERT INTO color (id_color, nombre) VALUES (11, 'NARANJA');
+INSERT INTO color (id_color, nombre) VALUES (12, 'CREMA/BEIGE');
+INSERT INTO color (id_color, nombre) VALUES (13, 'GRIS PERLA');
+INSERT INTO color (id_color, nombre) VALUES (14, 'CREMA');
+INSERT INTO color (id_color, nombre) VALUES (15, 'GRIS/AZUL');
+INSERT INTO color (id_color, nombre) VALUES (16, 'CREMA/DUA BEIGE');
 
 -- migrate:down
-DELETE FROM colors;
+DELETE FROM color;
