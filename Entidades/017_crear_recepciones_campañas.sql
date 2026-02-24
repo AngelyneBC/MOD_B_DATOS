bb3e170ce76248475208ea6baa85b879f033024d
AngelyneBC <angelynealvarez00@gmail.com>
2026-02-24 08:25:47 -0500

    correcciones id

--- Entidades/017_crear_recepciones_campañas.sql
+++ Entidades/017_crear_recepciones_campañas.sql
@@ -1,7 +1,7 @@
 CREATE TABLE recepciones_campañas (
     id_recepcion_campaña INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     recepcion_id INTEGER NOT NULL,
-    campaña_id VARCHAR(100) NOT NULL, 
+    campaña_id INTEGER NOT NULL,
 
     FOREIGN KEY (recepcion_id) REFERENCES recepciones(id_recepcion),
     FOREIGN KEY (campaña_id) REFERENCES campañas(id_campaña)
--- inserts_distritos.sql
+++ inserts_distritos.sql
@@ -1,21 +1,9 @@
 -- migrate:up
 
 INSERT INTO distritos (id_distrito, nombre) VALUES (1, 'ATE');
-INSERT INTO distritos (id_distrito, nombre) VALUES (2, 'CAMACHO');
-INSERT INTO distritos (id_distrito, nombre) VALUES (3, 'CAMACHO PATIO');
-INSERT INTO distritos (id_distrito, nombre) VALUES (4, 'COCHERA');
-INSERT INTO distritos (id_distrito, nombre) VALUES (5, 'EN MOLINA');
-INSERT INTO distritos (id_distrito, nombre) VALUES (6, 'FRONTIS');
-INSERT INTO distritos (id_distrito, nombre) VALUES (7, 'PATIO');
-INSERT INTO distritos (id_distrito, nombre) VALUES (8, 'PATIO CAMACHO');
-INSERT INTO distritos (id_distrito, nombre) VALUES (9, 'PATIO LM');
-INSERT INTO distritos (id_distrito, nombre) VALUES (10, 'PATIO SI');
-INSERT INTO distritos (id_distrito, nombre) VALUES (11, 'PROBLEMAS');
-INSERT INTO distritos (id_distrito, nombre) VALUES (12, 'RETIRO DIRECTO');
-INSERT INTO distritos (id_distrito, nombre) VALUES (13, 'SAN ISIDRO');
-INSERT INTO distritos (id_distrito, nombre) VALUES (14, 'SAN ISIDRO COCHERA ARAÑON EN EL PARACHOQUE DELANTERO');
-INSERT INTO distritos (id_distrito, nombre) VALUES (15, 'SAN ISIDRO SHOWROOM');
-INSERT INTO distritos (id_distrito, nombre) VALUES (16, 'UNIDAD EN PATIO SI');
+INSERT INTO distritos (id_distrito, nombre) VALUES (2, 'SAN ISIDRO');
+INSERT INTO distritos (id_distrito, nombre) VALUES (3, 'CAMACHO');
+INSERT INTO distritos (id_distrito, nombre) VALUES (4, 'MOLINA');
 
 -- migrate:down
 DELETE FROM distritos;
