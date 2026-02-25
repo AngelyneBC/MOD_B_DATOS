d98e4912410ff84b67ccdbbf037731ca3ce98f21
castroramireznicolvaleria-rgb <castroramireznicolvaleria@gmail.com>
2026-02-25 16:26:56 -0500

    ultimo commit

--- inserts_ubicaciones.sql
+++ inserts_ubicaciones.sql
@@ -1,23 +1,23 @@
 -- migrate:up
 
 INSERT INTO ubicaciones (id_ubicacion, nombre, distritos_id) VALUES (1, 'COCHERA', 3);
-INSERT INTO ubicaciones (id_ubicacion, nombre, distritos_id) VALUES (2, 'PATIO', 4);
-INSERT INTO ubicaciones (id_ubicacion, nombre, distritos_id) VALUES (3, 'COCHERA', 1);
-INSERT INTO ubicaciones (id_ubicacion, nombre, distritos_id) VALUES (4, 'COCHERA', 2);
-INSERT INTO ubicaciones (id_ubicacion, nombre, distritos_id) VALUES (5, 'SHOWROOM', 2);
-INSERT INTO ubicaciones (id_ubicacion, nombre, distritos_id) VALUES (6, 'RETIRO DIRECTO', 1);
-INSERT INTO ubicaciones (id_ubicacion, nombre, distritos_id) VALUES (7, 'PATIO', 2);
-INSERT INTO ubicaciones (id_ubicacion, nombre, distritos_id) VALUES (8, 'COCHERA', 4);
-INSERT INTO ubicaciones (id_ubicacion, nombre, distritos_id) VALUES (9, 'RETIRO DIRECTO', 4);
-INSERT INTO ubicaciones (id_ubicacion, nombre, distritos_id) VALUES (10, 'PATIO', 1);
-INSERT INTO ubicaciones (id_ubicacion, nombre, distritos_id) VALUES (11, 'PATIO', 3);
-INSERT INTO ubicaciones (id_ubicacion, nombre, distritos_id) VALUES (12, 'FRONTIS', 3);
-INSERT INTO ubicaciones (id_ubicacion, nombre, distritos_id) VALUES (13, 'RETIRO DIRECTO', 3);
-INSERT INTO ubicaciones (id_ubicacion, nombre, distritos_id) VALUES (14, 'RETIRO DIRECTO', 2);
-INSERT INTO ubicaciones (id_ubicacion, nombre, distritos_id) VALUES (15, 'SLA', 2);
-INSERT INTO ubicaciones (id_ubicacion, nombre, distritos_id) VALUES (16, 'SLA', 3);
-INSERT INTO ubicaciones (id_ubicacion, nombre, distritos_id) VALUES (17, 'SLA', 1);
-INSERT INTO ubicaciones (id_ubicacion, nombre, distritos_id) VALUES (18, 'SLA', 4);
+INSERT INTO ubicaciones (id_ubicacion, nombre, distritos_id) VALUES (2, 'COCHERA', 4);
+INSERT INTO ubicaciones (id_ubicacion, nombre, distritos_id) VALUES (3, 'PATIO', 4);
+INSERT INTO ubicaciones (id_ubicacion, nombre, distritos_id) VALUES (4, 'PATIO', 3);
+INSERT INTO ubicaciones (id_ubicacion, nombre, distritos_id) VALUES (5, 'COCHERA', 1);
+INSERT INTO ubicaciones (id_ubicacion, nombre, distritos_id) VALUES (6, 'SHOWROOM', 2);
+INSERT INTO ubicaciones (id_ubicacion, nombre, distritos_id) VALUES (7, 'RETIRO DIRECTO', 4);
+INSERT INTO ubicaciones (id_ubicacion, nombre, distritos_id) VALUES (8, 'COCHERA', 2);
+INSERT INTO ubicaciones (id_ubicacion, nombre, distritos_id) VALUES (9, 'PATIO', 1);
+INSERT INTO ubicaciones (id_ubicacion, nombre, distritos_id) VALUES (10, 'RETIRO DIRECTO', 2);
+INSERT INTO ubicaciones (id_ubicacion, nombre, distritos_id) VALUES (11, 'FRONTIS', 4);
+INSERT INTO ubicaciones (id_ubicacion, nombre, distritos_id) VALUES (12, 'RETIRO DIRECTO', 1);
+INSERT INTO ubicaciones (id_ubicacion, nombre, distritos_id) VALUES (13, 'SLA', 1);
+INSERT INTO ubicaciones (id_ubicacion, nombre, distritos_id) VALUES (14, 'SLA', 3);
+INSERT INTO ubicaciones (id_ubicacion, nombre, distritos_id) VALUES (15, 'RETIRO DIRECTO', 3);
+INSERT INTO ubicaciones (id_ubicacion, nombre, distritos_id) VALUES (16, 'SLA', 2);
+INSERT INTO ubicaciones (id_ubicacion, nombre, distritos_id) VALUES (17, 'PATIO', 2);
+INSERT INTO ubicaciones (id_ubicacion, nombre, distritos_id) VALUES (18, 'FRONTIS', 3);
 
 -- migrate:down
 DELETE FROM ubicaciones;
